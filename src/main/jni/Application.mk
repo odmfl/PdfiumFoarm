ccd8ccc52435e72ab884a58badde42b46b76b30e
Brian Mwadime <brian.mwadime@gmail.com>
2019-09-30 15:49:31 +0300

    update build file

--- src/main/jni/Application.mk
+++ src/main/jni/Application.mk
@@ -4,7 +4,6 @@ APP_CPPFLAGS += -fexceptions
 #For ANativeWindow support
 APP_PLATFORM = android-16
 
-APP_ABI :=  armeabi-v7a \
-            arm64-v8a \
+APP_ABI :=  arm64-v8a \
             x86 \
             x86_64
